6eb28b227b825b0e46179d41168ba3041f95bbda
frapecha <frapecha@evirw-gfs-shard.dsic.upv.es>
2018-02-12 19:52:59 +0100

    Ejemplo de GridPane y captura de eventos de teclado.

--- src/practica2ipc/FXMLDocumentController.java
+++ src/practica2ipc/FXMLDocumentController.java
@@ -11,6 +11,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 
 /**
  *
@@ -18,10 +22,12 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private GridPane gridPane;
+    @FXML
+    private Circle circle;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +37,27 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    private void teclaLevantada(KeyEvent event) {
+         Integer numCol = GridPane.getColumnIndex(circle);
+        Integer numRow = GridPane.getRowIndex(circle);
+        System.out.println("NumCol "+numCol);
+       
+        if(event.getCode() == KeyCode.UP){
+            if(numRow>0)
+           GridPane.setRowIndex(circle, numRow-1);
+           
+        }else if(event.getCode() == KeyCode.DOWN){
+           if(numRow<4)
+         GridPane.setRowIndex(circle, numRow+1);
+        }else if(event.getCode() == KeyCode.LEFT){
+           if(numCol>0)
+             GridPane.setColumnIndex(circle, numCol-1);
+        }else if(event.getCode() == KeyCode.RIGHT){
+            if(numCol<4)
+             GridPane.setColumnIndex(circle, numCol+1);
+        
+        }
+    }
 }
